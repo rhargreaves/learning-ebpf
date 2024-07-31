51d67ceffd41e1bf2bab57fa4a5d8d238b38c94e
Robert Hargreaves <kiyouta@gmail.com>
2024-07-31 22:10:34 +0100

    Remove debug flag

--- chapter2/hello-buffer.py
+++ chapter2/hello-buffer.py
@@ -32,7 +32,7 @@ int hello(void *ctx) {
 }
 """
 
-b = BPF(text=program, cflags=["-O1"])
+b = BPF(text=program)
 syscall = b.get_syscall_fnname("execve")
 b.attach_kprobe(event=syscall, fn_name="hello")
 
