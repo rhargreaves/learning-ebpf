b0d33de72c7d8eea171aea3245e7dabe09e51715
Robert Hargreaves <kiyouta@gmail.com>
2024-08-01 00:22:05 +0100

    Complete ch 2 ex 5

--- chapter2/hello-map.py
+++ chapter2/hello-map.py
@@ -5,18 +5,17 @@ from time import sleep
 program = r"""
 BPF_HASH(counter_table);
 
-int hello(void *ctx) {
-   u64 uid;
+int hello(struct bpf_raw_tracepoint_args *ctx) {
    u64 counter = 0;
    u64 *p;
+   u64 opcode = ctx->args[1];
 
-   uid = bpf_get_current_uid_gid() & 0xFFFFFFFF;
-   p = counter_table.lookup(&uid);
+   p = counter_table.lookup(&opcode);
    if (p != 0) {
       counter = *p;
    }
    counter++;
-   counter_table.update(&uid, &counter);
+   counter_table.update(&opcode, &counter);
 
    bpf_trace_printk("Boop");
    return 0;
