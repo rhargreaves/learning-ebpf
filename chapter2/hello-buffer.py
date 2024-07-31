a7bbb9e59dfb46441d8a4375aa9a932be29f138a
Robert Hargreaves <kiyouta@gmail.com>
2024-07-31 22:09:33 +0100

    Complete ch 2 ex 1

--- chapter2/hello-buffer.py
+++ chapter2/hello-buffer.py
@@ -1,40 +1,45 @@
-#!/usr/bin/python3  
+#!/usr/bin/python3
 from bcc import BPF
 
 program = r"""
-BPF_PERF_OUTPUT(output); 
- 
-struct data_t {     
+BPF_PERF_OUTPUT(output);
+
+struct data_t {
    int pid;
    int uid;
    char command[16];
    char message[12];
 };
- 
+
 int hello(void *ctx) {
-   struct data_t data = {}; 
-   char message[12] = "Hello World";
- 
+   struct data_t data = {};
+   char messageEven[12] = "Hello Even! ";
+   char messageOdd[12]  = "Hello Odd!  ";
+
    data.pid = bpf_get_current_pid_tgid() >> 32;
    data.uid = bpf_get_current_uid_gid() & 0xFFFFFFFF;
-   
+
    bpf_get_current_comm(&data.command, sizeof(data.command));
-   bpf_probe_read_kernel(&data.message, sizeof(data.message), message); 
- 
-   output.perf_submit(ctx, &data, sizeof(data)); 
- 
+   if(data.pid % 2 == 0) {
+      bpf_probe_read_kernel(&data.message, sizeof(data.message), messageEven);
+   } else {
+      bpf_probe_read_kernel(&data.message, sizeof(data.message), messageOdd);
+   }
+
+   output.perf_submit(ctx, &data, sizeof(data));
+
    return 0;
 }
 """
 
-b = BPF(text=program) 
+b = BPF(text=program, cflags=["-O1"])
 syscall = b.get_syscall_fnname("execve")
 b.attach_kprobe(event=syscall, fn_name="hello")
- 
-def print_event(cpu, data, size):  
+
+def print_event(cpu, data, size):
    data = b["output"].event(data)
    print(f"{data.pid} {data.uid} {data.command.decode()} {data.message.decode()}")
- 
-b["output"].open_perf_buffer(print_event) 
-while True:   
+
+b["output"].open_perf_buffer(print_event)
+while True:
    b.perf_buffer_poll()
