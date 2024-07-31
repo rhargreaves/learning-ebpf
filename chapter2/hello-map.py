3be7d2a1ef8c91911e71984a25cd05bd691f0fe4
Robert Hargreaves <kiyouta@gmail.com>
2024-07-31 22:58:56 +0100

    Complete ch 2 ex 2

--- chapter2/hello-map.py
+++ chapter2/hello-map.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3  
+#!/usr/bin/python3
 from bcc import BPF
 from time import sleep
 
@@ -17,15 +17,52 @@ int hello(void *ctx) {
    }
    counter++;
    counter_table.update(&uid, &counter);
+
+   bpf_trace_printk("Boop");
+   return 0;
+}
+
+int openat_hello(void *ctx) {
+   u64 uid;
+   u64 counter = 0;
+   u64 *p;
+
+   uid = bpf_get_current_uid_gid() & 0xFFFFFFFF;
+   p = counter_table.lookup(&uid);
+   if (p != 0) {
+      counter = *p;
+   }
+   counter++;
+   counter_table.update(&uid, &counter);
+
+   bpf_trace_printk("openat Boop");
+   return 0;
+}
+
+int write_hello(void *ctx) {
+   u64 uid;
+   u64 counter = 0;
+   u64 *p;
+
+   uid = bpf_get_current_uid_gid() & 0xFFFFFFFF;
+   p = counter_table.lookup(&uid);
+   if (p != 0) {
+      counter = *p;
+   }
+   counter++;
+   counter_table.update(&uid, &counter);
+
+   bpf_trace_printk("write Boop");
    return 0;
 }
 """
 
 b = BPF(text=program)
-syscall = b.get_syscall_fnname("execve")
-b.attach_kprobe(event=syscall, fn_name="hello")
+b.attach_kprobe(event=b.get_syscall_fnname("execve"), fn_name="hello")
+b.attach_kprobe(event=b.get_syscall_fnname("openat"), fn_name="openat_hello")
+b.attach_kprobe(event=b.get_syscall_fnname("write"), fn_name="write_hello")
 
-# Attach to a tracepoint that gets hit for all syscalls 
+# Attach to a tracepoint that gets hit for all syscalls
 # b.attach_raw_tracepoint(tp="sys_enter", fn_name="hello")
 
 while True:
